55089ce4fd3a248cbba86764afa881193810b39d
Kwoak <maisonneuve.henry@gmail.com>
2019-12-04 10:41:47 +0100

    :sparkles: (fight) add timer and victoryCheck to fight logic

--- js/game/logic/fight.js
+++ js/game/logic/fight.js
@@ -1,5 +1,42 @@
 class Fight {
-    constructor() {
-        
+  constructor(player1, player2, stage) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.stage = stage;
+    this.timer = 600;
+    this.player1Life = 1000;
+    this.player2Life = 1000;
+    this.winner;
+
+    this.update = game => {
+      this.timerDown();
+
+      // tout le reste de la logique pour retirer les pv
+
+      if (this.checkVictory()) {
+        game.gameState = game.gameStateEnum.ENDMENU;
+      }
+    };
+  }
+
+  timerDown = () => {
+    this.timer -= 1;
+  };
+
+  checkVictory = () => {
+    if (this.player1Life === 0 || (this.player1Life < this.player2Life && this.timer === 0)) {
+      this.winner = this.player2;
+      return true;
+    } else if (this.player2Life === 0 || (this.player2Life < this.player1Life && this.timer === 0)) {
+      this.winner = this.player1;
+      return true;
+    } else if (
+      (this.player1Life === 0 && this.player2Life === 0) ||
+      (this.player1Life === this.player2Life && this.timer === 0)
+    ) {
+      return true;
+    } else {
+      return false;
     }
+  };
 }
--- js/game/logic/game.js
+++ js/game/logic/game.js
@@ -1,72 +1,63 @@
 class Game {
-    constructor(inputList) {
-        this.frame = 0;
-        this.inputList = inputList;
+  constructor(inputList) {
+    this.frame = 0;
+    this.inputList = inputList;
 
-        this.menuOptionList = [
-            'playerVSplayer',
-            'playerVScomputer',
-            'training',
-            'settings',
-            'about'
-        ]
+    this.menuOptionList = ['playerVSplayer', 'playerVScomputer', 'training', 'settings', 'about'];
 
-        this.gameStateEnum = {
-            MAINMENU: 'mainMenu',
-            CHARACTERSELECTION: 'characterSelection',
-            FIGHT: 'fight',
-            ENDMENU: 'endMenu'
-        }
+    this.gameStateEnum = {
+      MAINMENU: 'mainMenu',
+      CHARACTERSELECTION: 'characterSelection',
+      FIGHT: 'fight',
+      ENDMENU: 'endMenu'
+    };
 
-        this.gameState = this.gameStateEnum.MAINMENU;
+    this.gameState = this.gameStateEnum.MAINMENU;
 
-        this.players = new Map();
+    this.players = new Map();
 
-        this.characterSelection = null;
+    this.characterSelection = null;
 
-        this.fight = null;
-        
-        this.mainMenuCursor = 0;
+    this.fight = null;
 
-        this.updateMainMenu = () => {
+    this.mainMenuCursor = 0;
 
-        }
+    this.updateMainMenu = () => {};
 
-        this.updateEndMenu = () => {
+    this.updateEndMenu = () => {};
 
-        }
-        
-        this.manageCharacterSelection = () => {
+    this.manageCharacterSelection = () => {};
 
-        }
+    this.manageFight = () => {
+      if (this.fight === null) {
+        this.fight = new Fight();
+      } else {
+        fight.update(this);
+      }
+    };
 
-        this.manageFight = () => {
+    this.update = () => {
+      switch (this.gameState) {
+        case gameStateEnum.MAINMENU:
+          this.updateMainMenu();
+        case gameStateEnum.CHARACTERSELECTION:
+          this.manageCharacterSelection();
+        case gameStateEnum.FIGHT:
+          this.manageFight();
+        case gameStateEnum.ENDMENU:
+          this.updateEndMenu();
+      }
 
-        }
+      // this.inputList.forEach((input, id) => {
+      //     if (input.a) console.log("a : " + id);
+      //     if (input.b) console.log("b : " + id);
+      //     if (input.up) console.log("up : " + id);
+      //     if (input.down) console.log("down : " + id);
+      //     if (input.left) console.log("left : " + id);
+      //     if (input.right) console.log("right : " + id);
+      // });
 
-        this.update = () => {
-
-            switch(this.gameState){
-                case gameStateEnum.MAINMENU:
-                    this.updateMainMenu();
-                case gameStateEnum.CHARACTERSELECTION:
-                    this.manageCharacterSelection();
-                case gameStateEnum.FIGHT:
-                    this.manageFight();
-                case gameStateEnum.ENDMENU:
-                    this.updateEndMenu();
-            }
-            
-            // this.inputList.forEach((input, id) => {
-            //     if (input.a) console.log("a : " + id);
-            //     if (input.b) console.log("b : " + id);
-            //     if (input.up) console.log("up : " + id);
-            //     if (input.down) console.log("down : " + id);
-            //     if (input.left) console.log("left : " + id);
-            //     if (input.right) console.log("right : " + id);
-            // });
-
-            this.frame++;
-        }
-    }
+      this.frame++;
+    };
+  }
 }
