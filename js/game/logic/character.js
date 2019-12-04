9898b25d582ab3ef069da6f2d6f7d877aa8489ef
Kwoak <maisonneuve.henry@gmail.com>
2019-12-04 11:57:36 +0100

    :sparkles: (character) add args

--- js/game/logic/character.js
+++ js/game/logic/character.js
@@ -1,6 +1,10 @@
 class Character {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-    }
+  constructor(id, name, pos, size, walkSpeed, jumpSpeed) {
+    this.id = id;
+    this.name = name;
+    this.pos = pos;
+    this.size = size;
+    this.walkSpeed = walkSpeed;
+    this.jumpSpeed = jumpSpeed;
+  }
 }
