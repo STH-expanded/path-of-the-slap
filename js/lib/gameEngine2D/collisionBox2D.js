0b2354ecb30bfc35c1fb49ccee2ddd600a02f168
Kwoak <maisonneuve.henry@gmail.com>
2019-12-04 11:42:42 +0100

    :recycle: (collisions) refactor into arrows functions

--- js/lib/gameEngine2D/collisionBox2D.js
+++ js/lib/gameEngine2D/collisionBox2D.js
@@ -1,29 +1,29 @@
-function isCollide(playerPos, playerSize, wallPos, wallSize) {
+var isCollide = (playerPos, playerSize, wallPos, wallSize) => {
   return !(
     playerPos.y + playerSize.y < wallPos.y ||
     playerPos.y > wallPos.y + wallSize.y ||
     playerPos.x + playerSize.x < wallPos.x ||
     playerPos.x > wallPos.x + wallSize.x
   );
-}
+};
 
-function isIntersect(playerPos, playerSize, wallPos, wallSize) {
+var isIntersect = (playerPos, playerSize, wallPos, wallSize) => {
   return !(
     playerPos.y + playerSize.y <= wallPos.y ||
     playerPos.y >= wallPos.y + wallSize.y ||
     playerPos.x + playerSize.x <= wallPos.x ||
     playerPos.x >= wallPos.x + wallSize.x
   );
-}
+};
 
-function inBound(playerPos, playerSize, bound) {
+var inBound = (playerPos, playerSize, bound) => {
   return !(
     playerPos.y + playerSize.y > bound.size.y / 2 ||
     playerPos.y < -bound.size.y / 2 ||
     playerPos.x + playerSize.x > bound.size.x / 2 ||
     playerPos.x < -bound.size.x / 2
   );
-}
+};
 
 var obstaclesAt = (pos, size, obstacles) => {
   var result = [];
