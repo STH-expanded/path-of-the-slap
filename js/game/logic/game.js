f1c7bdcf1b11e03f6b629f4f31cf5db8a46e4b2f
samy <samy_leni@hotmail.fr>
2019-12-04 09:49:31 +0100

    feat: game state

--- js/game/logic/game.js
+++ js/game/logic/game.js
@@ -11,32 +11,60 @@ class Game {
             'about'
         ]
 
-        this.gamemode = null;
+        this.gameStateEnum = {
+            MAINMENU: 'mainMenu',
+            CHARACTERSELECTION: 'characterSelection',
+            FIGHT: 'fight',
+            ENDMENU: 'endMenu'
+        }
+
+        this.gameState = this.gameStateEnum.MAINMENU;
 
         this.players = new Map();
 
+        this.characterSelection = null;
+
         this.fight = null;
         
+        this.mainMenuCursor = 0;
+
+        this.updateMainMenu = () => {
+
+        }
+
+        this.updateEndMenu = () => {
+
+        }
+        
+        this.manageCharacterSelection = () => {
+
+        }
+
+        this.manageFight = () => {
+
+        }
+
         this.update = () => {
 
-            if (!this.gamemode) {
-                
-            }
-            else if (!this.fight) {
-                    
-            }
-            else {
-
+            switch(this.gameState){
+                case gameStateEnum.MAINMENU:
+                    this.updateMainMenu();
+                case gameStateEnum.CHARACTERSELECTION:
+                    this.manageCharacterSelection();
+                case gameStateEnum.FIGHT:
+                    this.manageFight();
+                case gameStateEnum.ENDMENU:
+                    this.updateEndMenu();
             }
             
-            this.inputList.forEach((input, id) => {
-                if (input.a) console.log("a : " + id);
-                if (input.b) console.log("b : " + id);
-                if (input.up) console.log("up : " + id);
-                if (input.down) console.log("down : " + id);
-                if (input.left) console.log("left : " + id);
-                if (input.right) console.log("right : " + id);
-            });
+            // this.inputList.forEach((input, id) => {
+            //     if (input.a) console.log("a : " + id);
+            //     if (input.b) console.log("b : " + id);
+            //     if (input.up) console.log("up : " + id);
+            //     if (input.down) console.log("down : " + id);
+            //     if (input.left) console.log("left : " + id);
+            //     if (input.right) console.log("right : " + id);
+            // });
 
             this.frame++;
         }
