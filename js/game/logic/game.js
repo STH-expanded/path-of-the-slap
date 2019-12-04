0ab4a4b0cc3cc9b1b1cf8a66600d9e9afcfd17bf
leorigaudeau <leo.rigaudeau@ynov.com>
2019-12-04 13:52:18 +0100

    init des lastInputs

--- js/game/logic/game.js
+++ js/game/logic/game.js
@@ -1,63 +1,91 @@
 class Game {
-  constructor(inputList) {
-    this.frame = 0;
-    this.inputList = inputList;
+    constructor(inputList) {
+        this.frame = 0;
+        this.inputList = inputList;
+        this.lastInputList = new Map();
 
-    this.menuOptionList = ['playerVSplayer', 'playerVScomputer', 'training', 'settings', 'about'];
+        this.menuOptionList = ['playerVSplayer', 'playerVScomputer', 'training', 'settings', 'about'];
+        this.endMenuOptionList = [ 'training', 'settings', 'about'];
 
-    this.gameStateEnum = {
-      MAINMENU: 'mainMenu',
-      CHARACTERSELECTION: 'characterSelection',
-      FIGHT: 'fight',
-      ENDMENU: 'endMenu'
-    };
 
-    this.gameState = this.gameStateEnum.MAINMENU;
+        this.gameStateEnum = {
+            MAINMENU: 'mainMenu',
+            CHARACTERSELECTION: 'characterSelection',
+            FIGHT: 'fight',
+            ENDMENU: 'endMenu'
+        };
 
-    this.players = new Map();
+        this.gameState = this.gameStateEnum.ENDMENU;
 
-    this.characterSelection = null;
+        this.players = new Map();
 
-    this.fight = null;
+        this.characterSelection = null;
 
-    this.mainMenuCursor = 0;
+        this.fight = null;
 
-    this.updateMainMenu = () => {};
+        this.mainMenuCursor = 0;
+        this.EndMenuCursor = 0;
 
-    this.updateEndMenu = () => {};
+        this.updateMainMenu = () => { };
 
-    this.manageCharacterSelection = () => {};
+        this.updateEndMenu = () => {
+            this.inputList.forEach((input, id) => {
+                this.lastInputList.forEach((lastinput,lastid)=>{
+                    if (id=lastid && !lastinput.a && !lastinput.down && !lastinput.up) {
+                        if (input.a) {
+                            console.log("a : " + id);
+                            this.doEnter
+                        }
+                        if (input.up) {
+                            this.EndMenuCursor = (((this.EndMenuCursor - 1)%this.endMenuOptionList.length)+this.endMenuOptionList.length) % this.endMenuOptionList.length
+                            console.log("up : " + this.EndMenuCursor);
+                        }
+                        if (input.down) {
+                            this.EndMenuCursor = (((this.EndMenuCursor + 1)%this.endMenuOptionList.length)+this.endMenuOptionList.length) % this.endMenuOptionList.length
+                            console.log("down : " + this.EndMenuCursor);
+                        }
+                    }
+                })
+                
+            }, this);
+            console.log(this.endMenuOptionList[this.EndMenuCursor])
+        };
 
-    this.manageFight = () => {
-      if (this.fight === null) {
-        this.fight = new Fight();
-      } else {
-        fight.update(this);
-      }
-    };
+        this.manageCharacterSelection = () => { };
 
-    this.update = () => {
-      switch (this.gameState) {
-        case gameStateEnum.MAINMENU:
-          this.updateMainMenu();
-        case gameStateEnum.CHARACTERSELECTION:
-          this.manageCharacterSelection();
-        case gameStateEnum.FIGHT:
-          this.manageFight();
-        case gameStateEnum.ENDMENU:
-          this.updateEndMenu();
-      }
+        this.manageFight = () => {
+            if (this.fight === null) {
+                this.fight = new Fight();
+            } else {
+                fight.update(this);
+            }
+        };
 
-      // this.inputList.forEach((input, id) => {
-      //     if (input.a) console.log("a : " + id);
-      //     if (input.b) console.log("b : " + id);
-      //     if (input.up) console.log("up : " + id);
-      //     if (input.down) console.log("down : " + id);
-      //     if (input.left) console.log("left : " + id);
-      //     if (input.right) console.log("right : " + id);
-      // });
+        this.update = () => {
+            switch (this.gameState) {
+                case this.gameStateEnum.MAINMENU:
+                    this.updateMainMenu();
+                case this.gameStateEnum.CHARACTERSELECTION:
+                    this.manageCharacterSelection();
+                case this.gameStateEnum.FIGHT:
+                    this.manageFight();
+                case this.gameStateEnum.ENDMENU:
+                    this.updateEndMenu();
+            }
 
-      this.frame++;
-    };
-  }
+            // this.inputList.forEach((input, id) => {
+            //     if (input.a) console.log("a : " + id);
+            //     if (input.b) console.log("b : " + id);
+            //     if (input.up) console.log("up : " + id);
+            //     if (input.down) console.log("down : " + id);
+            //     if (input.left) console.log("left : " + id);
+            //     if (input.right) console.log("right : " + id);
+            // });
+            this.inputList.forEach((input, id) => {
+                this.lastInputList.set(id, JSON.parse(JSON.stringify(input)));
+            });
+
+            this.frame++;
+        };
+    }
 }
