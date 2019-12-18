87302c5c59ade91c053fee86ba3fa15ca24b09b1
leorigaudeau <leo.rigaudeau@ynov.com>
2019-12-18 11:29:30 +0100

    Mise en place du responsive

--- js/game/display/GUI.js
+++ js/game/display/GUI.js
@@ -3,35 +3,37 @@ class GUI {
         this.update =  (fight, cx, zoom)=>{
             cx.fillStyle = 'white';
             // x,y,w,h
-            // TIMER
-            cx.fillRect((cx.canvas.width/2)-50, 5 , 100, 100);
+            // TIMER 
+            cx.fillRect((cx.canvas.width/2)-20*zoom, 5*zoom , 40*zoom, 40*zoom);
             cx.fillStyle = 'black';
-            cx.font = "55px serif";
+            cx.font = 25*zoom+"px serif";
             cx.textAlign = "center";
             var timer =(fight.timer- (fight.timer % 60))/60;
-            cx.fillText(timer.toString(),(cx.canvas.width/2), 70);
+            cx.fillText(timer.toString(),(cx.canvas.width/2), 32*zoom);
             
             // PV
             // P1
             cx.fillStyle = 'black';
-            cx.fillRect(10, 5 ,50 , 50);
+            cx.fillRect(10, 5 ,20*zoom , 20*zoom);
             cx.fillStyle = 'white';
-            cx.fillRect((cx.canvas.width/4)-(cx.canvas.width/6), 5 , (cx.canvas.width/3) , 45);
+            cx.fillRect((cx.canvas.width/4)-(cx.canvas.width/6), 5 , (cx.canvas.width/3) , 20*zoom);
             cx.fillStyle = 'orange';
-            cx.fillRect(((cx.canvas.width/4)-(cx.canvas.width/6))+((cx.canvas.width/3)-((cx.canvas.width/3)*(fight.player1.character.health /fight.player1.character.maxHealth))) , 5 , (cx.canvas.width/3)*(fight.player1.character.health /fight.player1.character.maxHealth) , 45);
+            cx.fillRect(((cx.canvas.width/4)-(cx.canvas.width/6))+((cx.canvas.width/3)-((cx.canvas.width/3)*(fight.player1.character.health /fight.player1.character.maxHealth))) , 5 , (cx.canvas.width/3)*(fight.player1.character.health /fight.player1.character.maxHealth) , 20*zoom);
             cx.fillStyle = 'black';
-            cx.font = "18px serif";
+            cx.font = 10*zoom+"px serif";
             cx.textAlign = "start";
-            cx.fillText(fight.player1.character.name,(cx.canvas.width/4)-(cx.canvas.width/6), 70);
+            cx.fillText(fight.player1.character.name,(cx.canvas.width/4)-(cx.canvas.width/6), 35*zoom);
             //P2    
             cx.fillStyle = 'black';
-            cx.fillRect(cx.canvas.width-60, 5 , 50 , 50);
+            cx.fillRect(cx.canvas.width-30*zoom, 5 , 20*zoom , 20*zoom);
             cx.fillStyle = 'white';
-            cx.fillRect(((cx.canvas.width*3)/4)-(cx.canvas.width/6), 5 , (cx.canvas.width/3) , 45);
+            cx.fillRect(((cx.canvas.width*3)/4)-(cx.canvas.width/6), 5 , (cx.canvas.width/3) , 20*zoom);
             cx.fillStyle = 'purple';
+            cx.fillRect(((cx.canvas.width*3)/4)-(cx.canvas.width/6), 5 , (cx.canvas.width/3)*(fight.player1.character.health /fight.player1.character.maxHealth)  , 20*zoom);
+            cx.fillStyle = 'black';
             cx.textAlign = "end";
-            cx.fillRect(((cx.canvas.width*3)/4)-(cx.canvas.width/6), 5 , (cx.canvas.width/3)*(fight.player1.character.health /fight.player1.character.maxHealth)  , 45);
-            cx.fillText(fight.player1.character.name,((cx.canvas.width*3)/4)+(cx.canvas.width/6), 70);
+            cx.font = 10*zoom+"px serif";
+            cx.fillText(fight.player1.character.name,((cx.canvas.width*3)/4)+(cx.canvas.width/6), 35*zoom);
         }     
     }
 }
