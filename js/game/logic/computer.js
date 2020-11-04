3932a6cd95bb0b82d2d34af5fe55cc9a59adab14
Geoffrey_max <geoffrey.clermont@ynov.com>
2020-11-04 15:39:07 +0100

    feat(cpu): add ia a

--- js/game/logic/computer.js
+++ js/game/logic/computer.js
@@ -1,22 +1,43 @@
 class Computer extends Player {
     constructor() {
         super();
-        this.inputComputer = {
-            left: false,
-            up: false,
-            right: false,
-            down: false,
-            a: false,
-            b: false,
-            start: false
+        this.sequenceIndex = 0; //frame
+        this.sequence = {
+            frames: 60,
+            steps: {
+                0: {
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: true,
+                    b: false,
+                    start: false
+                },
+                1: {
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                }
+            }
         }
-
     }
 
-    getInput = activity => {
+    getInput = (activity) => {
+        let input = null;
+
         if (activity instanceof Fight) {
-            this.inputComputer.a = true;
+            input = this.sequence.steps[Object.keys(this.sequence.steps).reverse().find(step => step <= this.sequenceIndex)];
         }
-        return this.inputComputer;
+
+        this.sequenceIndex++;
+        if (this.sequenceIndex > this.sequence.frames) this.sequenceIndex = 0;
+
+
+        return input;
     }
 }
