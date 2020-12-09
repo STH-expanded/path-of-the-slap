f8eb1205f8b0297f74e7e075c9a1090d87836444
Geoffrey_max <geoffrey.clermont@ynov.com>
2020-12-09 09:33:51 +0100

    computer modify

--- js/game/logic/computer.js
+++ js/game/logic/computer.js
@@ -3,6 +3,7 @@ class Computer extends Player {
         super();
         this.sequenceIndex = 0; //frame
         this.sequences = [{
+            //lowA + highA + highB
             frames: 65,
             fit: (fight, character) => {
                 const p1 = fight.players[0].character.collisionBox;
@@ -78,8 +79,15 @@ class Computer extends Player {
             }
         },
         {
-            frames: 60,
-            fit: (fight, character) => (0),
+            // lowB + Reculer + QCF
+            frames: 100,
+            fit: (fight, character) => {
+                const p1 = fight.players[0].character.collisionBox;
+                const p2 = fight.players[1].character.collisionBox;
+                const dist = Math.abs(p1.pos.x + p1.size.x / 2 - (p2.pos.x + p2.size.x / 2)) - p1.size.x / 2 - p2.size.x / 2;
+                const hit_dist = 80 - character.collisionBox.size.x / 2;
+                return hit_dist >= dist ? 1 : 0;
+            },
             steps: {
                 0: (fight, character) => ({
                     left: false,
@@ -107,10 +115,56 @@ class Computer extends Player {
                     a: false,
                     b: false,
                     start: false
+                }),
+                33: (fight, character) => ({
+                    left: character.direction,
+                    up: false,
+                    right: !character.direction,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                }),
+                64: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                }),
+                65: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: true,
+                    a: false,
+                    b: false,
+                    start: false
+                }),
+                66: (fight, character) => ({
+                    left: !character.direction,
+                    up: false,
+                    right: character.direction,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                }),
+                67: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: true,
+                    b: false,
+                    start: false
                 })
             }
         },
         {
+            // QCF + dash
             frames: 60,
             fit: (fight, character) => .05,
             steps: {
@@ -124,9 +178,9 @@ class Computer extends Player {
                     start: false
                 }),
                 1: (fight, character) => ({
-                    left: true,
+                    left: !character.direction,
                     up: false,
-                    right: false,
+                    right: character.direction,
                     down: false,
                     a: false,
                     b: false,
@@ -187,7 +241,187 @@ class Computer extends Player {
                     start: false
                 })
             }
-        }];
+        },
+        {
+            // AerialB + HighA + HighA + HighB
+            frames: 90,
+            fit: (fight, character) => {
+                const p1 = fight.players[0].character.collisionBox;
+                const p2 = fight.players[1].character.collisionBox;
+                const dist = Math.abs(p1.pos.x + p1.size.x / 2 - (p2.pos.x + p2.size.x / 2)) - p1.size.x / 2 - p2.size.x / 2;
+                const hit_dist = 80 - character.collisionBox.size.x / 2;
+                return hit_dist >= dist ? 1 : 0;
+            },
+            steps: {
+                0: (fight, character) => ({
+                    left: false,
+                    up: true,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                }),
+                1: (fight, character) => ({
+                    left: false,
+                    up: true,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: true,
+                    start: false
+                }),
+                2: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                }),
+                25: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: true,
+                    b: false,
+                    start: false
+                }),
+                26: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                }),
+                41: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: true,
+                    b: false,
+                    start: false
+                }),
+                42: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                }),
+                57: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: true,
+                    start: false
+                }),
+                58: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                })
+            }
+        },
+        {
+            // LowB + AerialA + HighB
+            frames: 90,
+            fit: (fight, character) => {
+                const p1 = fight.players[0].character.collisionBox;
+                const p2 = fight.players[1].character.collisionBox;
+                const dist = Math.abs(p1.pos.x + p1.size.x / 2 - (p2.pos.x + p2.size.x / 2)) - p1.size.x / 2 - p2.size.x / 2;
+                const hit_dist = 80 - character.collisionBox.size.x / 2;
+                return hit_dist >= dist ? 1 : 0;
+            },
+            steps: {
+                0: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: true,
+                    a: false,
+                    b: false,
+                    start: false
+                }),
+                1: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: true,
+                    a: false,
+                    b: true,
+                    start: false
+                }),
+                2: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                }),
+                32: (fight, character) => ({
+                    left: false,
+                    up: true,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                }),
+                33: (fight, character) => ({
+                    left: false,
+                    up: true,
+                    right: false,
+                    down: false,
+                    a: true,
+                    b: false,
+                    start: false
+                }),
+                34: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                }),
+                59: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: true,
+                    start: false
+                }),
+                60: (fight, character) => ({
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false,
+                    a: false,
+                    b: false,
+                    start: false
+                })
+            }
+        }
+        ];
         this.sequence = this.sequences[0];
     }
 
